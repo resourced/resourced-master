9fa2c67731e6dae0f4a77f5333c8b210633ea757
Didip Kerabat <didipk@gmail.com>
2015-01-15 21:31:46 -0800

    libstring

---
+++ libstring/libstring.go
@@ -0,0 +1,44 @@
+package libstring
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"os"
+	"os/user"
+	"strings"
+)
+
+// ExpandTilde is a convenience function that expands ~ to full path.
+func ExpandTilde(path string) string {
+	usr, _ := user.Current()
+	homeDir := usr.HomeDir
+
+	if path[:2] == "~/" {
+		path = strings.Replace(path, "~", homeDir, 1)
+	}
+	return path
+}
+
+// ExpandTilde is a convenience function that expands both ~ and $ENV.
+func ExpandTildeAndEnv(path string) string {
+	path = ExpandTilde(path)
+	return os.ExpandEnv(path)
+}
+
+// GeneratePassword returns password.
+// size determines length of initial seed bytes.
+func GeneratePassword(size int) (string, error) {
+	// Force minimum size to 32
+	if size < 32 {
+		size = 32
+	}
+
+	rb := make([]byte, size)
+	_, err := rand.Read(rb)
+
+	if err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(rb), nil
+}
