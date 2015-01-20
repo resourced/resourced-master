cdfd46aeab43652be9faa61a2d141a6cab709446
Didip Kerabat <didipk@gmail.com>
2015-01-19 23:53:52 -0800

    Flesh out the server.

--- resourced-master.go
+++ resourced-master.go
@@ -2,21 +2,53 @@ package main
 
 import (
 	"fmt"
-	"github.com/codegangsta/negroni"
+	"github.com/carbocation/interpose"
+	"github.com/carbocation/interpose/middleware"
+	resourcedmaster_dao "github.com/resourced/resourced-master/dao"
 	"github.com/resourced/resourced-master/libenv"
+	resourcedmaster_storage "github.com/resourced/resourced-master/storage"
 	"net/http"
 )
 
+func storage() (resourcedmaster_storage.Storer, error) {
+	env := libenv.EnvWithDefault("RESOURCED_MASTER_ENV", "test")
+	s3AccessKey := libenv.EnvWithDefault("RESOURCED_MASTER_S3_ACCESS_KEY", "")
+	s3SecretKey := libenv.EnvWithDefault("RESOURCED_MASTER_S3_SECRET_KEY", "")
+	s3Region := libenv.EnvWithDefault("RESOURCED_MASTER_S3_REGION", "us-east-1")
+	s3Bucket := libenv.EnvWithDefault("RESOURCED_MASTER_S3_BUCKET", "")
+
+	return resourcedmaster_storage.NewS3(env, s3AccessKey, s3SecretKey, s3Region, s3Bucket), nil
+}
+
+func middlewareStruct(store resourcedmaster_storage.Storer) (*interpose.Middleware, error) {
+	accessTokens, err := resourcedmaster_dao.AllAccessTokens(store)
+	if err != nil {
+		return nil, err
+	}
+
+	middle := interpose.New()
+
+	for _, accessToken := range accessTokens {
+		println(accessToken.Token)
+		middle.Use(middleware.BasicAuth(accessToken.Token, ""))
+	}
+
+	return middle, nil
+}
+
 func main() {
+	store, _ := storage()
+
+	middle, _ := middlewareStruct(store)
+
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "Welcome to the home page!")
 	})
 
+	middle.UseHandler(mux)
+
 	serverAddress := libenv.EnvWithDefault("RESOURCED_MASTER_ADDR", ":55655")
-
-	n := negroni.Classic()
-	n.UseHandler(mux)
-
-	http.ListenAndServe(serverAddress, n)
+	http.ListenAndServe(serverAddress, middle)
 }
