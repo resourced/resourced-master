7f23b1804fa40013b153dda177e399a2190e454f
Didip Kerabat <didipk@gmail.com>
2015-01-19 23:53:22 -0800

    Able to get list of filenames from S3.

--- storage/s3.go
+++ storage/s3.go
@@ -72,7 +72,19 @@ func (store *S3) Get(fullpath string) ([]byte, error) {
 }
 
 func (store *S3) List(fullpath string) ([]string, error) {
+	fullpath = path.Join(store.Root, fullpath)
+
+	response, err := store.Bucket.List(fullpath, "", "", 500)
+	if err != nil {
+		return nil, err
+	}
+
 	var result []string
+
+	for _, object := range response.Contents {
+		result = append(result, path.Base(object.Key))
+	}
+
 	return result, nil
 }
 
