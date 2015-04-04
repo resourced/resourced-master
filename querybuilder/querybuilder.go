e578d37be3c2ac9983a006ace34799ef06541806
Didip Kerabat <didipk@gmail.com>
2015-04-04 09:48:27 -0700

    Small refactor.

--- querybuilder/querybuilder.go
+++ querybuilder/querybuilder.go
@@ -1,3 +1,4 @@
+// Package querybuilder provides query building functionality.
 package querybuilder
 
 import (
@@ -6,84 +7,19 @@ import (
 	"strings"
 )
 
+// Parse parses ResourceD query and turns it into postgres query.
 func Parse(input string) string {
+	return parseAnd(input)
+}
+
+func parseAnd(input string) string {
 	pgQueryParts := make([]string, 0)
 
 	statements := strings.Split(input, " AND ")
 	for _, statement := range statements {
-		statement = strings.TrimSpace(statement)
-
-		// Querying tags.
-		// There can only be 1 operator for tags: "="
-		if strings.HasPrefix(statement, "Tags") || strings.HasPrefix(statement, "tags") {
-			parts := strings.Split(statement, "=")
-
-			arrayOfTagsInString := parts[len(parts)-1]
-			arrayOfTagsInString = strings.TrimSpace(arrayOfTagsInString)
-			arrayOfTagsInString = libstring.StripChars(arrayOfTagsInString, "[]")
-
-			query := fmt.Sprintf("tags ?& array[%v]", arrayOfTagsInString)
-
-			pgQueryParts = append(pgQueryParts, query)
-		}
-
-		// Querying name.
-		// Operators:
-		// "="  : Exact match.
-		// "~^" : Starts with.
-		if strings.HasPrefix(statement, "Name") || strings.HasPrefix(statement, "name") {
-			if strings.Contains(statement, "=") {
-				parts := strings.Split(statement, "=")
-
-				name := parts[len(parts)-1]
-				name = strings.TrimSpace(name)
-
-				query := fmt.Sprintf("name = %v", name)
-				pgQueryParts = append(pgQueryParts, query)
-
-			} else if strings.Contains(statement, "~^") {
-				parts := strings.Split(statement, "~^")
-
-				name := parts[len(parts)-1]
-				name = strings.TrimSpace(name)
-				name = libstring.StripChars(name, `"'`)
-
-				query := `name LIKE "` + name + `%"`
-				pgQueryParts = append(pgQueryParts, query)
-			}
-		}
-
-		// Querying data.
-		// Operators: >=, <=, =, <, >
-		// Expected output: data #>> '{/free,Swap,Free}' = '0'
-		if strings.HasPrefix(statement, "/") {
-			operator := ""
-
-			if strings.Contains(statement, ">=") {
-				operator = ">="
-			} else if strings.Contains(statement, "<=") {
-				operator = "<="
-			} else if strings.Contains(statement, "=") {
-				operator = "="
-			} else if strings.Contains(statement, "<") {
-				operator = ">"
-			} else if strings.Contains(statement, ">") {
-				operator = ">"
-			}
-
-			if operator != "" {
-				parts := strings.Split(statement, operator)
-
-				pgJsonPath := strings.Replace(parts[0], ".", ",", -1)
-				pgJsonPath = strings.TrimSpace(pgJsonPath)
-
-				value := parts[len(parts)-1]
-				value = strings.TrimSpace(value)
-				value = libstring.StripChars(value, `"'`)
-
-				query := fmt.Sprintf("data #>> '{%v}' %v '%v'", pgJsonPath, operator, value)
-				pgQueryParts = append(pgQueryParts, query)
-			}
+		pgStatement := parseStatement(statement)
+		if pgStatement != "" {
+			pgQueryParts = append(pgQueryParts, pgStatement)
 		}
 	}
 
@@ -91,6 +27,81 @@ func Parse(input string) string {
 		return strings.Join(pgQueryParts, " AND ")
 	} else if len(pgQueryParts) == 1 {
 		return pgQueryParts[0]
+	}
+
+	return ""
+}
+
+// parseStatement parses ResourceD statement and turns it into postgres statement.
+func parseStatement(statement string) string {
+	statement = strings.TrimSpace(statement)
+
+	// Querying tags.
+	// There can only be 1 operator for tags: "="
+	if strings.HasPrefix(statement, "Tags") || strings.HasPrefix(statement, "tags") {
+		parts := strings.Split(statement, "=")
+
+		arrayOfTagsInString := parts[len(parts)-1]
+		arrayOfTagsInString = strings.TrimSpace(arrayOfTagsInString)
+		arrayOfTagsInString = libstring.StripChars(arrayOfTagsInString, "[]")
+
+		return fmt.Sprintf("tags ?& array[%v]", arrayOfTagsInString)
+	}
+
+	// Querying name.
+	// Operators:
+	// "="  : Exact match.
+	// "~^" : Starts with.
+	if strings.HasPrefix(statement, "Name") || strings.HasPrefix(statement, "name") {
+		if strings.Contains(statement, "=") {
+			parts := strings.Split(statement, "=")
+
+			name := parts[len(parts)-1]
+			name = strings.TrimSpace(name)
+
+			return fmt.Sprintf("name = %v", name)
+
+		} else if strings.Contains(statement, "~^") {
+			parts := strings.Split(statement, "~^")
+
+			name := parts[len(parts)-1]
+			name = strings.TrimSpace(name)
+			name = libstring.StripChars(name, `"'`)
+
+			return `name LIKE "` + name + `%"`
+		}
+	}
+
+	// Querying data.
+	// Operators: >=, <=, =, <, >
+	// Expected output: data #>> '{/free,Swap,Free}' = '0'
+	if strings.HasPrefix(statement, "/") {
+		operator := ""
+
+		if strings.Contains(statement, ">=") {
+			operator = ">="
+		} else if strings.Contains(statement, "<=") {
+			operator = "<="
+		} else if strings.Contains(statement, "=") {
+			operator = "="
+		} else if strings.Contains(statement, "<") {
+			operator = ">"
+		} else if strings.Contains(statement, ">") {
+			operator = ">"
+		}
+
+		if operator != "" {
+			parts := strings.Split(statement, operator)
+
+			pgJsonPath := strings.Replace(parts[0], ".", ",", -1)
+			pgJsonPath = strings.TrimSpace(pgJsonPath)
+
+			value := parts[len(parts)-1]
+			value = strings.TrimSpace(value)
+			value = libstring.StripChars(value, `"'`)
+
+			return fmt.Sprintf("data #>> '{%v}' %v '%v'", pgJsonPath, operator, value)
+		}
 	}
 
 	return ""
