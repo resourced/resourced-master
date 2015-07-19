5d3d1521c88ff42eb16c487e8b0158d10acc502a
Didip Kerabat <didip@newrelic.com>
2015-07-18 23:16:01 -0700

    finally fixed the bad handshake

--- handlers/ws.go
+++ handlers/ws.go
@@ -27,6 +27,10 @@ const (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	// CheckOrigin == true allows CORS permission.
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func setWSConnection(w http.ResponseWriter, r *http.Request, conn *websocket.Conn) {
