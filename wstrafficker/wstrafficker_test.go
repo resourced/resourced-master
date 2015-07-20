f111f1540e658dbf5715219a57543e7da941eb56
Didip Kerabat <didip@newrelic.com>
2015-07-19 19:15:33 -0700

    basic test

--- wstrafficker/wstrafficker_test.go
+++ wstrafficker/wstrafficker_test.go
@@ -4,6 +4,13 @@ import (
 	"testing"
 )
 
-func TestBasic(t *testing.T) {
-	New()
+func TestConstructor(t *testing.T) {
+	trafficker := NewWSTrafficker(nil)
+
+	if trafficker.Chans.Send == nil {
+		t.Errorf("Send channel should not be nil")
+	}
+	if trafficker.Chans.Receive == nil {
+		t.Errorf("Receive channel should not be nil")
+	}
 }
