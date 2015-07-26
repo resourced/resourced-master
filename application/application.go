7342992ae64a3490cce75a47912a3348214dbdd3
Didip Kerabat <didip@newrelic.com>
2015-07-26 09:48:34 -0700

    do not ignore slash for key-value

--- application/application.go
+++ application/application.go
@@ -105,8 +105,8 @@ func (app *Application) mux() *mux.Router {
 	router.Handle("/api/hosts", alice.New(MustLoginApi).ThenFunc(handlers.PostApiHosts)).Methods("POST")
 
 	router.Handle("/api/metadata", alice.New(MustLoginApi).ThenFunc(handlers.GetApiMetadata)).Methods("GET")
-	router.Handle("/api/metadata/{key}", alice.New(MustLoginApi).ThenFunc(handlers.PostApiMetadataKey)).Methods("POST")
-	router.Handle("/api/metadata/{key}", alice.New(MustLoginApi).ThenFunc(handlers.GetApiMetadataKey)).Methods("GET")
+	router.Handle(`/api/metadata/{key:[\w\/\-\_]+}`, alice.New(MustLoginApi).ThenFunc(handlers.PostApiMetadataKey)).Methods("POST")
+	router.Handle(`/api/metadata/{key:[\w\/\-\_]+}`, alice.New(MustLoginApi).ThenFunc(handlers.GetApiMetadataKey)).Methods("GET")
 
 	// Path of static files must be last!
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("static")))
