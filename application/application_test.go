0fd84936cb57494ac436f83a0015109e7de625a8
Didip Kerabat <didipk@gmail.com>
2016-01-12 00:27:20 -0800

    Simple tests for application

---
+++ application/application_test.go
@@ -0,0 +1,17 @@
+package application
+
+import (
+	"os"
+	"testing"
+)
+
+func TestConstructor(t *testing.T) {
+	app, err := New("../tests/config-files")
+	if err != nil {
+		t.Errorf("Creating a new app should not fail using test config. Error: %v", err)
+	}
+	hostname, _ := os.Hostname()
+	if app.Hostname != hostname {
+		t.Errorf("Failed to configure hostname properly")
+	}
+}
