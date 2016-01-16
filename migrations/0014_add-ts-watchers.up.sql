63c67b46a64aefb357ce5bdefbe90003dcac6752
Didip Kerabat <didipk@gmail.com>
2016-01-16 07:55:33 -0800

    Redo timeseries schema.

--- migrations/0014_add-ts-watchers.up.sql
+++ migrations/0014_add-ts-watchers.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ts_watchers (
-    cluster_id bigint REFERENCES clusters (id),
-    watcher_id bigint REFERENCES watchers (id),
+    cluster_id bigint,
+    watcher_id bigint,
     created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
     affected_hosts bigint NOT NULL DEFAULT 0,
     data json
--- migrations/0018_add-ts-metrics.up.sql
+++ migrations/0018_add-ts-metrics.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ts_metrics (
-    cluster_id bigint REFERENCES clusters (id),
-    metric_id bigint REFERENCES metrics (id),
+    cluster_id bigint,
+    metric_id bigint,
     created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
     key TEXT NOT NULL,
     host TEXT NOT NULL,
