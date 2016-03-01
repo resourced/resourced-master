186414404ab517738a256ccdd952a5f9b4c9e9ff
Lydia Ngo <lydia.liman@gmail.com>
2016-03-01 00:15:59 -0800

    Generate up-to-date default config file on first time install.

--- config/default.go
+++ config/default.go
@@ -1,9 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/resourced/resourced/libstring"
@@ -35,16 +37,18 @@ Addr = "localhost:55655"
 # Valid LogLevel are: debug, info, warning, error, fatal, panic
 LogLevel = "info"
 
-# List of all instances hostnames
-Hosts = []
-
-# DSN to PostgreSQL
+# DSN to core PostgreSQL database
 DSN = "postgres://localhost:5432/resourced-master?sslmode=disable"
 
 RequestTimeout = "1s"
 
 # Change this!
 CookieSecret = "T0PS3CR3T"
+
+# In production, change this to your VIP host and port
+VIPAddr = "localhost:55655"
+
+VIPProtocol = "http"
 
 [HTTPS]
 # Path to HTTPS cert file
@@ -53,19 +57,78 @@ CertFile = ""
 # Path to HTTPS key file
 KeyFile = ""
 
+[Email]
+From = "dontreply@example.com"
+SubjectPrefix = "[ResourceDMaster]"
+Host = "smtp.gmail.com"
+Port = 587
+Username = ""
+Password = ""
+Identity = ""
+
+[Metrics]
+DSNs = [
+    "postgres://localhost:5432/resourced-master-ts-metrics-1?sslmode=disable",
+    "postgres://localhost:5432/resourced-master-ts-metrics-2?sslmode=disable"
+]
+
+# Valid values for ReplicationPercentage are: 0 < x <= 100
+# ReplicationPercentage == 100 means that every time series data is replicated to 100% of databases defined in DSNs.
+# ReplicationPercentage == 50 means that every time series data is replicated to 50% of databases defined in DSNs.
+# ReplicationPercentage == 0 is invalid, it will be converted to 100 instead.
+ReplicationPercentage = 100
+
+# DataRetention defines how long time series data are kept.
+# The unit is defined in days.
+DataRetention = 7
+
 [Watchers]
 ListFetchInterval = "60s"
+DSNs = [
+    "postgres://localhost:5432/resourced-master-ts-watchers-1?sslmode=disable",
+    "postgres://localhost:5432/resourced-master-ts-watchers-2?sslmode=disable"
+]
+
+# DataRetention defines how long time series data are kept.
+# The unit is defined in days.
+DataRetention = 1
 
 [Watchers.Email]
 From = "alert@example.com"
-Subject = ""
-Host = "smtp.example.com"
-Port = 25
+SubjectPrefix = "[ERROR]"
+Host = "smtp.gmail.com"
+Port = 587
 Username = ""
 Password = ""
+Identity = ""
+
+[Watchers.SMSEmailGateway]
+att = "txt.att.net"
+alltel = "message.alltel.com"
+sprint = "messaging.sprintpcs.com"
+tmobile = "tmomail.com"
+verizon = "vtext.com"
+virgin = "vmobl.com"
+
+[Events]
+DSNs = [
+    "postgres://localhost:5432/resourced-master-ts-events-1?sslmode=disable",
+    "postgres://localhost:5432/resourced-master-ts-events-2?sslmode=disable"
+]
+
+# DataRetention defines how long time series data are kept.
+# The unit is defined in days.
+DataRetention = 7
 `
 
-	err := ioutil.WriteFile(path.Join(configDir, "general.toml"), []byte(generalToml), 0644)
+	// Generate totally random cookie secret
+	randomCookieSecret, err := libstring.GeneratePassword(32)
+	if err != nil {
+		return err
+	}
+	generalToml = strings.Replace(generalToml, `CookieSecret = "T0PS3CR3T"`, fmt.Sprintf(`CookieSecret = "%v"`, randomCookieSecret), 1)
+
+	err = ioutil.WriteFile(path.Join(configDir, "general.toml"), []byte(generalToml), 0644)
 	if err != nil {
 		return err
 	}
