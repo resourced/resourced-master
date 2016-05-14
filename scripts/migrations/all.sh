fbb04c42f829e2e9b6c8c8234a05801281b4c67a
Lydia Ngo <lydia.liman@gmail.com>
2016-05-14 09:51:11 -0700

    Migration script for all databases.

---
+++ scripts/migrations/all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PGUSER=${PGUSER:-$(whoami)}
+PGHOST=${PGHOST:-"localhost"}
+PGPORT=${PGPORT:-"5432"}
+PGSSLMODE=${PGSSLMODE:-"disable"}
+
+migrate -url postgres://$PGUSER@$PGHOST:$PGPORT/resourced-master?sslmode=$PGSSLMODE -path $CURRENT_DIR/../../migrations/core $@
+migrate -url postgres://$PGUSER@$PGHOST:$PGPORT/resourced-master-test?sslmode=$PGSSLMODE -path $CURRENT_DIR/../../migrations/core $@
+
+migrate -url postgres://$PGUSER@$PGHOST:$PGPORT/resourced-master-ts-events?sslmode=$PGSSLMODE -path $CURRENT_DIR/../../migrations/ts-events $@
+
+migrate -url postgres://$PGUSER@$PGHOST:$PGPORT/resourced-master-ts-metrics?sslmode=$PGSSLMODE -path $CURRENT_DIR/../../migrations/ts-metrics $@
+
+migrate -url postgres://$PGUSER@$PGHOST:$PGPORT/resourced-master-ts-executor-logs?sslmode=$PGSSLMODE -path $CURRENT_DIR/../../migrations/ts-executor-logs $@
+
+migrate -url postgres://$PGUSER@$PGHOST:$PGPORT/resourced-master-ts-logs?sslmode=$PGSSLMODE -path $CURRENT_DIR/../../migrations/ts-logs $@
+
+migrate -url postgres://$PGUSER@$PGHOST:$PGPORT/resourced-master-ts-metrics?sslmode=$PGSSLMODE -path $CURRENT_DIR/../../migrations/ts-metrics $@
+
+migrate -url postgres://$PGUSER@$PGHOST:$PGPORT/resourced-master-ts-checks?sslmode=$PGSSLMODE -path $CURRENT_DIR/../../migrations/ts-checks $@
