204ba035231cfca456d7d57f2944ab5a77b498fa
Didip Kerabat <didipk@gmail.com>
2016-08-21 22:50:31 -0700

    Display error message using toastr. In this commit, we are using it for validating email.

--- static/resourced-master/js/dashboard.js
+++ static/resourced-master/js/dashboard.js
@@ -611,3 +611,25 @@ Highcharts.theme = {
 };
 
 Highcharts.setOptions(Highcharts.theme);
+
+// ---------------------------------------
+// Toaster Settings
+// ---------------------------------------
+
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
